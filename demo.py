b3f7d2218c9fd797fef4aa4b4dee56d5a0c625b1
Anubhav2017 <agarwalanubhav.2011@gmail.com>
2020-04-11 17:44:54 +0530

    started pset2

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
 import random
 
-# Code Sample A
-mylist = []
-
-random.seed(0)
-for i in range(10):
-    print(random.randint(1,10))
+random.seed(9001)
+d = random.randint(1, 10)#1
+for i in range(random.randint(1, 10)):#5   5 2 10
+    if random.randint(1, 10) < 7:
+        print(d)
+    else:
+        random.seed(9001)
+        d = random.randint(1, 10) #1
+        print(random.randint(1, 10)) 
